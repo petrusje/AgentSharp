a1d55e4a07e82d54e81715a172f3197d899d6de1
Yurinabaia <35046482+Yurinabaia@users.noreply.github.com>
2025-08-18 15:34:14 +0000

    att file

--- ThirdParty/HNSW.Net/Src/HNSW.Net/BinaryHeap.cs
+++ ThirdParty/HNSW.Net/Src/HNSW.Net/BinaryHeap.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="BinaryHeap.cs" company="Microsoft">
+// <copyright file="BinaryHeap.cs" company="Microsoft">
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 // </copyright>
--- ThirdParty/HNSW.Net/Src/HNSW.Net/SmallWorld.Utils.cs
+++ ThirdParty/HNSW.Net/Src/HNSW.Net/SmallWorld.Utils.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="SmallWorld.Utils.cs" company="Microsoft">
+// <copyright file="SmallWorld.Utils.cs" company="Microsoft">
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 // </copyright>
